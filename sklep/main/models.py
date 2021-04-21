bbcf7728113599429e7e47d32743d7cb516b16d7
Marceli Koćwin <988marceli@gmail.com>
2021-04-21 21:29:02 +0200

    dodano pare przykładów dla wytłumaczenia i pokazania funkcjonalności

---
+++ sklep/main/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=250)
+    release = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
