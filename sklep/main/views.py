cb98408027c50a779b20cc7d526c80bc45c89d1e
Marceli Koćwin <988marceli@gmail.com>
2021-05-14 19:36:42 +0200

    jakieś zmiany

--- sklep/main/views.py
+++ sklep/main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from .models import Movie
+
 # Create your views here.
+
+def list_movies(request):
+    movies = Movie.objects.all()
+    m = Movie.objects.get(id=1)
+    context = {'movies': movies, 'm':m}
+    return render(request, 'base.html', context)
