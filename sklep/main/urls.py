d992f738856d66aecb68fb16e3229933edc7dbdb
Marceli Koćwin <988marceli@gmail.com>
2021-05-15 23:08:03 +0200

    zmiany w gitignore

--- sklep/main/urls.py
+++ sklep/main/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.list_movies, name='list_movies'),
+    path('<int:id>', views.list_movies, name='list_movies'),
 ]
--- sklep/main/views.py
+++ sklep/main/views.py
@@ -4,8 +4,8 @@ from .models import Movie
 
 # Create your views here.
 
-def list_movies(request):
+def list_movies(request, id):
     movies = Movie.objects.all()
-    m = Movie.objects.get(id=1)
+    m = Movie.objects.get(id=id)
     context = {'movies': movies, 'm':m}
     return render(request, 'base.html', context)
