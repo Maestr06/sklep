b65791d4b29f68e673acfdae48b6711d29fcd239
Marceli Koćwin <988marceli@gmail.com>
2021-06-20 07:55:37 +0200

    added model for codes for movies

--- sklep/main/models.py
+++ sklep/main/models.py
@@ -1,4 +1,8 @@
+import uuid
+from datetime import timedelta
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
 class Movie(models.Model):
@@ -7,3 +11,11 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
+
+class Code(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    value = models.UUIDField(primary_key=True, 
+    unique_for_date=timedelta(days=30), default=uuid.uuid4, 
+    editable=False)
+    active_time = models.DurationField(default=timedelta(days=30))
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
